cbb4f0c3a55c3833058e4f4db8eb1cfec2c0a36d
Enzo. <enzo.ferreira.carvalho@escola.pr.gov.br>
2025-10-29 12:19:41 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+//Importando arquivos dos React
+import React, { useState} from "react";
+import { View, Text,TextInput, Button , StyleSheet } from "react-native";
+
+//Criando o principal componente do app
+export default function App(){
+  //Armazenar o nome do usuário
+  const [name , setName] = useState('');
+
+  //Armazena a saudação do usuário
+  const[great , setGreat] = useState ('');
+}
